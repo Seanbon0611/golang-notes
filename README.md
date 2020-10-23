77fdf2b82cd7b0331f50861e89324ca00c96cdcc
Seanbon0611 <seand0611@gmail.com>
2020-10-22 22:57:15 -0700

    initiating notes for learning GO

---
+++ README.md
@@ -0,0 +1,1 @@
+My Notes for Learning the GO Language
---
+++ hello_world.go
@@ -0,0 +1,14 @@
+//main is the package used for all applications
+package main
+
+//import statement to add additional libraries
+import (
+	//fmt allows us to format strings
+	"fmt"
+	// "rsc.io/quote"
+)
+
+func main() {
+	// fmt.Println(quote.Go())
+	fmt.Println("Hello, World!")
+}
